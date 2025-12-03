1b60cbd9d9c4ba9b8da80a58d2b992257449b725
PedroDeSanti <phmartinsanti@gmail.com>
2025-12-03 14:42:21 -0300

    ci: update documentation structure and add new sections for Computação, Elétrica, and Mecânica

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,26 +7,31 @@ export default defineConfig({
   site: "https://tcc.desanti.dev/",
   integrations: [
     starlight({
-      title: "My Docs",
+      title: "Micras",
       social: [
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/withastro/starlight",
+          href: "https://github.com/team-micras",
         },
       ],
       sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
+        // {
+        //   label: "Guides",
+        //   items: [
+        //     // Each item here is one entry in the navigation menu.
+        //     { label: "Example Guide", slug: "guides/example" },
+
+        //   ],
+        // },
+        // {
+        //   label: "Reference",
+        //   autogenerate: { directory: "reference" },
+        // },
+
+        { label: "Computação", slug: "computação" },
+        { label: "Elétrica", slug: "elétrica" },
+        { label: "Mecânica", slug: "mecânica" },
       ],
     }),
   ],
